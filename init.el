3f3245016cefeff19d028f948578b579772dbf6f
Florian Bauer <fbauer.devel@gmail.com>
2014-07-19 16:07:48 +0200

    Save pending changes

--- init.el
+++ init.el
@@ -36,14 +36,14 @@
  '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "be7eadb2971d1057396c20e2eebaa08ec4bfd1efe9382c12917c6fe24352b7c1" default)))
- '(imenu-auto-rescan t t)
+ '(imenu-auto-rescan t)
  '(js2-auto-indent-p nil)
  '(js2-bounce-indent-p t)
  '(js2-cleanup-whitespace t)
  '(js2-mode-indent-ignore-first-tab t)
  '(menu-bar-mode t)
- '(python-remove-cwd-from-path nil)
-)
+ '(org-agenda-files (quote ("~/orgmode/logins.org.gpg")))
+ '(python-remove-cwd-from-path nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -74,3 +74,6 @@
 
 ;; my own instaparse mode
 (require 'instaparse-mode)
+;; work-around for dead key problem
+;; see http://www.emacswiki.org/emacs/DeadKeys
+(require 'iso-transl)
